8407babd98852fc719df3d3b879407c366b7d93a
nikhilrawat2005 <nikhil2005114@gmail.com>
2026-09-21 13:37:05 +0530

    server-external-packages for firebase-admin and node-cron

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["firebase-admin", "node-cron"],
+};
 
 export default nextConfig;
